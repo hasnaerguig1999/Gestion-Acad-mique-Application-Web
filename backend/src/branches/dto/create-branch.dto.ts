a8e988a513d41ad24b07a90ecf0d735cce91e207
hasnaerguig1999 <hasnaerguig1999@gmail.com>
2023-12-29 16:35:16 +0100

    modification de crud faculty et l'ajout de crud filére avec des petit changement sur app module

--- backend/src/branches/dto/create-branch.dto.ts
+++ backend/src/branches/dto/create-branch.dto.ts
@@ -1,1 +1,13 @@
-export class CreateBranchDto {}
+import { IsNotEmpty, IsString, MinLength } from "class-validator";
+
+export class CreateBranchDto {
+    @IsString()
+    @MinLength(4,{message:"name must be at least 4 characters"})
+    @IsNotEmpty()
+    name: string;
+
+    @IsString()
+    @MinLength(4,{message:"description must be at least 4 characters"})
+    @IsNotEmpty()
+    description: string;
+}
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -15,6 +15,7 @@ async function bootstrap() {
       }
     )
   )
+  
 
   await app.listen(3000);
 }
