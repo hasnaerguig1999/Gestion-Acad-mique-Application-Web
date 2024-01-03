6a58f0f73e2f284c2f3535af6aeff45b609946b0
hasnaerguig1999 <hasnaerguig1999@gmail.com>
2024-01-03 12:17:27 +0100

    modification sur findone et update et delete sur faculty et bfiliére et matiére

--- backend/src/subjects/dto/create-subject.dto.ts
+++ backend/src/subjects/dto/create-subject.dto.ts
@@ -1,8 +1,7 @@
-import { IsNotEmpty, IsString, MinLength } from "class-validator";
+import { IsNotEmpty, IsString } from "class-validator";
 
 export class CreateSubjectDto {
     @IsString()
-    @MinLength(4,{message:"name must be at least 4 characters"})
     @IsNotEmpty()
     name: string;
 }
