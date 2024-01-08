fbd53968758c05f6e2888c285cc58981220c2a2d
ismail <houssniismail6@gmail.com>
2024-01-08 09:33:03 +0100

    modification en subject

--- backend/src/subjects/dto/create-subject.dto.ts
+++ backend/src/subjects/dto/create-subject.dto.ts
@@ -4,11 +4,14 @@ import { IsInt, IsNotEmpty, IsNumber, IsString, MinLength } from "class-validato
 
 
 export class CreateSubjectDto {
+    
     @IsString()
     @IsNotEmpty()
     name: string;
+    
     @IsNumber()
     @IsNotEmpty()
     @IsInt({ message: "branchId must be an integer" })
     branchId:number
 }
+
